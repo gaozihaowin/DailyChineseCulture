a701335aff470cd27de5be68e26405684c49492b
GaoZihao <cmy9551@163.com>
2026-02-05 23:46:50 +0800

    同步api规范

--- api/config.js
+++ api/config.js
@@ -4,21 +4,21 @@
 // 导出API配置
 export const API_CONFIG = {
   // 本地开发环境地址
-  baseUrl: 'http://127.0.0.1:3000',
+  //baseUrl: 'http://127.0.0.1:8080',
   
   // Apifox 提供的云端 Mock 地址（备用）
-  // baseUrl: 'http://127.0.0.1:4523/m1/7779396-7525646-default',
+   baseUrl: 'http://127.0.0.1:4523/m1/7779396-7525646-default',
   
   // API路径配置
   paths: {
-    login: '/api/login',
-    hotCourses: '/api/courses/hot',
-    coursesIng: '/api/courses/ing',
-    coursesHistory: '/api/courses/history',
-    coursesDone: '/api/courses/done',
-    userInfo: '/api/user/info',
-    switchIdentity: '/api/user/switch-identity',
-	wechatLogin:'/api//wxLogin',
+    login: '/login',
+    hotCourses: '/courses/hot',
+    coursesIng: '/courses/ing',
+    coursesHistory: '/courses/history',
+    coursesDone: ' /courses/done',
+    userInfo: '/user/info',
+    switchIdentity: '/user/switch-identity',
+	wechatLogin:'//wxLogin',
     // 可以在此添加其他API路径
   }
 };
